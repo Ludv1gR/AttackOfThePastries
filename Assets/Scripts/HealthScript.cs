518371ec3cf952a422e017e5981138623e42fd99
Ludvig Rasmuson <ludderasmuson@gmail.com>
2024-10-11 00:52:49 +0200

    Enemies drop currency

--- Assets/Scripts/HealthScript.cs
+++ Assets/Scripts/HealthScript.cs
@@ -6,6 +6,7 @@ public class HealthScript : MonoBehaviour
 {
     [Header("Attributes")]
     [SerializeField] private int hitPoints = 2;
+    [SerializeField] private int currencyDrop = 30;
 
     private bool isDestroyed = false;
 
@@ -14,6 +15,7 @@ public class HealthScript : MonoBehaviour
 
         if(hitPoints <= 0 && !isDestroyed) {
             EnemySpawner.onEnemyDestroy.Invoke();
+            LevelManager.main.IncreaseCurrency(currencyDrop);
             isDestroyed = true;
             Destroy(gameObject);
         }
