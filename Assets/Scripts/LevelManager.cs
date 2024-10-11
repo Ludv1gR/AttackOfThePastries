02279b99d528af7eb2639235c9111a9f18656f4c
Ludvig Rasmuson <ludderasmuson@gmail.com>
2024-10-11 06:04:17 +0200

    playerHealth införd, lägg in game over nu bara

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LevelManager : MonoBehaviour
 {
 
     public static LevelManager main;
 
+    [Header("References")]
+    [SerializeField] TextMeshProUGUI playerHealthUI;
+
     public Transform startPoint;
     public Transform[] path;
 
     public int currency;
+    public int playerHealth;
 
     private void Awake()
     {
@@ -19,10 +24,23 @@ public class LevelManager : MonoBehaviour
 
     private void Start() {
         currency = 100;
+        playerHealth = 5; // TODO: 10
     }
 
     public void IncreaseCurrency(int amount) {
         currency += amount;
+    }
+
+    public void playerHealthLoss() {
+        playerHealth -= 1;
+
+        if(playerHealth <= 0) {
+
+            Debug.Log("YOU DIED");
+            //Svart screen långsamt
+            // Videon spelas och sedan svart skärm med Game Over
+            // några sekunder senare man tas tillbaka till StartMenu scene
+        }
     }
 
     public bool SpendCurrency(int amount) {
@@ -33,5 +51,9 @@ public class LevelManager : MonoBehaviour
             Debug.Log("You do not have neough to purchase this item!");
             return false;
         }
+    }
+
+    private void OnGUI() {
+        playerHealthUI.text = playerHealth.ToString();
     }
 }
